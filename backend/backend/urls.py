1c8cbd12903697bdb64afa2a40e3109b937b4a9d
Mithun Thomas <63800900+mithun2003@users.noreply.github.com>
2023-07-03 22:53:55 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     # path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('todo_api/', include('todo_api.urls')),
     path('', include('user.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
