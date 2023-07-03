d816ce9ed6c6d0af86d2087a96ccbad0e07d01c4
Mithun Thomas <mithunc.y65@gmail.com>
2023-07-03 22:38:08 +0530

    change the backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,8 +13,7 @@ import environ
 from pathlib import Path
 from datetime import timedelta
 import os
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
